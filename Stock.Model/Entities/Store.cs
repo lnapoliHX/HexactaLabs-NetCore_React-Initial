c4ef6987c7b6b9cf6c112b538e04ddaa3544f47b
Tomas Pulenta <tpulenta@gmail.com>
2020-06-20 19:30:51 -0300

    Initial - Se agrega al backend el modulo Provider

--- Stock.Model/Entities/Store.cs
+++ Stock.Model/Entities/Store.cs
@@ -4,9 +4,9 @@ namespace Stock.Model.Entities
 {
     public class Store : IEntity
     {
-        public string Id { get; set; }
 
         public string Name { get; set; }
+        public string Id { get; set; }
         
         public string Phone { get; set; }
 

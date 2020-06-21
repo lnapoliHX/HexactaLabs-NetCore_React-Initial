33e1586b4d036f5ad278aed88a0ad2a15bc190bc
Laymar Quintero <lquintero@hexacta.com>
2020-06-20 21:01:07 -0300

    Actividad Inicial

---
+++ Stock.Api/DTOs/ProviderSearchDTO.cs
@@ -0,0 +1,24 @@
+﻿using Stock.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stock.Api.DTOs
+{
+    public class ProviderSearchDTO
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public List<Product> OfferedProducts { get; set; }
+
+        public ActionDto Condition { get; set; }
+    }
+ 
+}
--- Stock.Api/Startup.cs
+++ Stock.Api/Startup.cs
@@ -36,7 +36,7 @@ namespace Stock.Api
             services.Configure<DomainSettings>(Configuration.GetSection("DomainSettings"));
             services.AddTransient<StoreService>();
             //services.AddTransient<ProductService>();
-            //services.AddTransient<ProviderService>();
+            services.AddTransient<ProviderService>();
             services.AddTransient<ProductTypeService>();
             services.AddTransient<Repository.LiteDb.Configuration.ConfigurationProvider>();
             services.AddTransient<ILiteConfiguration, LiteConfiguration>();
@@ -45,6 +45,7 @@ namespace Stock.Api
             services.AddTransient<IRepository<Product>, BaseRepository<Product>>();
             services.AddTransient<IRepository<ProductType>, BaseRepository<ProductType>>();
             services.AddTransient<IRepository<Store>, BaseRepository<Store>>();
+            
 
             services.AddAutoMapper();
 
@@ -62,13 +63,13 @@ namespace Stock.Api
 
         private void OnShutdown()
         {
-           // MySqlConnection.ClearAllPools();
+            // MySqlConnection.ClearAllPools();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(
-            IApplicationBuilder app, 
-            IHostingEnvironment env, 
+            IApplicationBuilder app,
+            IHostingEnvironment env,
             IApplicationLifetime applicationLifetime,
             ILoggerFactory loggerFactory)
         {
@@ -95,10 +96,10 @@ namespace Stock.Api
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Stock API V1");
-               // c.RoutePrefix = "docs";
+                // c.RoutePrefix = "docs";
             });
 
-            applicationLifetime.ApplicationStopping.Register(OnShutdown);                        
+            applicationLifetime.ApplicationStopping.Register(OnShutdown);
             app.UseMvc();
         }
     }

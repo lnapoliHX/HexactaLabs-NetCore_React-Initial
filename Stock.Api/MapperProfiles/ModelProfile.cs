8ae758d0faed8e5aa59c8b84c47d4f2af35824fc
marcelocarrasco <marceloacarrasco@gmail.com>
2020-06-21 22:32:29 -0300

    Marcelo Carrasco Hx Lab, Initial

--- Stock.Api/MapperProfiles/ModelProfile.cs
+++ Stock.Api/MapperProfiles/ModelProfile.cs
@@ -4,24 +4,24 @@ using Stock.Model.Entities;
 
 namespace Stock.Api.MapperProfiles
 {
-    public class ModelProfile : Profile
+    public class file : Profile
     {
-        public ModelProfile()
+        public file()
         {
             CreateMap<ProductType, ProductTypeDTO>()
                 //.IgnoreAllNonExisting()
                 .ReverseMap()
                 .ForMember(s => s.Id, opt => opt.Ignore());
 
-            // CreateMap<Product, ProductDTO>()
+             CreateMap<Product, ProductDTO>()
             //     .ForMember(d => d.ProductTypeId, opt => opt.MapFrom(s => s.ProductType.Id))
             //     .ForMember(d => d.ProductTypeDesc, opt => opt.MapFrom(s => s.ProductType.Description))
-            //     .ReverseMap()
+                 .ReverseMap();
             //     .ForMember(s => s.Id, opt => opt.Ignore())
             //     .ForMember(s => s.ProductType, opt => opt.Ignore());       
 
-            // CreateMap<Provider, ProviderDTO>()
-            //     .ReverseMap();         
+             CreateMap<Provider, ProviderDTO>()
+                 .ReverseMap();         
         }        
     }
 

86a4095ef7cc05f6a50f3fbc57ec354a36fd29d1
Ezequiel <eze_ruiz_pacios@hotmail.com>
2020-09-07 10:52:03 -0300

    Add attributes

--- Stock.Api/DTOs/ProviderDto.cs
+++ Stock.Api/DTOs/ProviderDto.cs
@@ -4,6 +4,12 @@ namespace Stock.Api.DTOs
 {
     public class ProviderDTO
     {
+        public string Id { get; set; }
 
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
     }
 }

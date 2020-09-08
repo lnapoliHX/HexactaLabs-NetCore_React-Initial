782ff39e3790976b60b43929ce1fc6a28a184044
Ariel Pironti <arielpironti@hotmail.com>
2020-09-08 14:34:29 -0300

    Add ProviderController.cs Add ProviderDTO.cs Add ProviderService.cs

---
+++ Stock.Api/DTOs/ProviderDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stock.Api.DTOs
+{
+    public class ProviderDTO
+    {
+        public string Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+    }
+}

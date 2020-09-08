0332a7a4ed82e1323976a040e7d8f5943700ef4f
JesusELuna <jesusemanuelluna@gmail.com>
2020-09-08 20:43:11 -0300

    entrega

---
+++ Stock.Api/DTOs/ProviderDTO.cs
@@ -0,0 +1,13 @@
+namespace Stock.Api.DTOs
+{
+  public class ProviderDTO
+  {
+    public string Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Phone { get; set; }
+
+    public string Email { get; set; }
+  }
+}
--- Stock.Api/MapperProfiles/ModelProfile.cs
+++ Stock.Api/MapperProfiles/ModelProfile.cs
@@ -4,26 +4,26 @@ using Stock.Model.Entities;
 
 namespace Stock.Api.MapperProfiles
 {
-    public class ModelProfile : Profile
+  public class ModelProfile : Profile
+  {
+    public ModelProfile()
     {
-        public ModelProfile()
-        {
-            CreateMap<ProductType, ProductTypeDTO>()
-                //.IgnoreAllNonExisting()
-                .ReverseMap()
-                .ForMember(s => s.Id, opt => opt.Ignore());
+      CreateMap<ProductType, ProductTypeDTO>()
+          //.IgnoreAllNonExisting()
+          .ReverseMap()
+          .ForMember(s => s.Id, opt => opt.Ignore());
 
-            // CreateMap<Product, ProductDTO>()
-            //     .ForMember(d => d.ProductTypeId, opt => opt.MapFrom(s => s.ProductType.Id))
-            //     .ForMember(d => d.ProductTypeDesc, opt => opt.MapFrom(s => s.ProductType.Description))
-            //     .ReverseMap()
-            //     .ForMember(s => s.Id, opt => opt.Ignore())
-            //     .ForMember(s => s.ProductType, opt => opt.Ignore());       
+      // CreateMap<Product, ProductDTO>()
+      //     .ForMember(d => d.ProductTypeId, opt => opt.MapFrom(s => s.ProductType.Id))
+      //     .ForMember(d => d.ProductTypeDesc, opt => opt.MapFrom(s => s.ProductType.Description))
+      //     .ReverseMap()
+      //     .ForMember(s => s.Id, opt => opt.Ignore())
+      //     .ForMember(s => s.ProductType, opt => opt.Ignore());       
 
-            // CreateMap<Provider, ProviderDTO>()
-            //     .ReverseMap();         
-        }        
+      CreateMap<Provider, ProviderDTO>()
+          .ReverseMap();
     }
+  }
 
 
 }

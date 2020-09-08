38ac9cad990176b8eb182034a6618b4b5906429d
Andres Ramirez <andresramirez82@gmail.com>
2020-09-08 19:58:03 -0300

    andres ramirez

--- Stock.Model/Entities/Provider.cs
+++ Stock.Model/Entities/Provider.cs
@@ -8,9 +8,9 @@ namespace Stock.Model.Entities
         public string Id { get; set; }
 
         public string Name { get; set; }
-        
-        public string Phone { get; set; }
 
+        public string Phone { get; set; }
+        
         public string Email { get; set; }
 
         public List<Product> OfferedProducts { get; set; }

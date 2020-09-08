0998e8258ad59a9da7d51fa969ad6b271837da65
jcastillo <castillo.jorge.08@gmail.com>
2020-09-08 20:47:18 -0300

    delete Required Field phone and email

--- Stock.Api/DTOs/ProviderDTO.cs
+++ Stock.Api/DTOs/ProviderDTO.cs
@@ -8,9 +8,7 @@ namespace Stock.Api.DTOs
 
         [Required]
         public string Name { get; set; }
-        [Required]
         public string Phone { get; set; }
-        [Required]
         public string Email { get; set; }  
     }
 }

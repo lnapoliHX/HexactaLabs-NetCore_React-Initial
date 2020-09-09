de3060328e01a9ffe7d01e82d1cfa02d190ec101
Mauricio <mauricioarizaga@hotmail.com>
2020-09-09 15:24:48 -0300

    Tarea Terminada. Chequeada

---
+++ Stock.Api/DTOs/ProviderSearchDTO.cs
@@ -0,0 +1,11 @@
+namespace Stock.Api.DTOs
+{
+    public class ProviderSearchDTO
+    {
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+
+        public ActionDto Condition { get; set; }
+    }
+}
--- Stock.Web/client-app/src/modules/providers/form/validation.js
+++ Stock.Web/client-app/src/modules/providers/form/validation.js
@@ -3,11 +3,8 @@ import "../../../common/helpers/YupConfig";
 
 const schema = yup.object().shape({
   name: yup.string().required(),
-  email: yup
-    .string()
-    .email()
-    .required(),
-  phone: yup.string().required()
+  email: yup.string().required(),
+  phone: yup.string().required(),
 });
 
 export default schema;

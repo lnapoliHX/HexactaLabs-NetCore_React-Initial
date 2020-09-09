dc5e22f702e859e7b5214aebb771142607766f88
Ezequiel <eze_ruiz_pacios@hotmail.com>
2020-09-09 15:08:57 -0300

    nothing

--- Stock.Api/DTOs/ProviderDto.cs
+++ Stock.Api/DTOs/ProviderDto.cs
@@ -4,12 +4,10 @@ namespace Stock.Api.DTOs
 {
     public class ProviderDTO
     {
-
         public string Id { get; set; }
-
         [Required]
         public string Name { get; set; }
-
         public string Phone { get; set; }
+        public string Email { get; set; }
     }
 }

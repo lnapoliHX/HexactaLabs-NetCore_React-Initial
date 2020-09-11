1801d12162cf082898b217ba4fc0a25d60a3ae69
Fernando Marín <62115870+marinfernando@users.noreply.github.com>
2020-09-11 19:49:24 -0300

    Inicial

--- Stock.Api/Startup.cs
+++ Stock.Api/Startup.cs
@@ -35,8 +35,8 @@ namespace Stock.Api
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.Configure<DomainSettings>(Configuration.GetSection("DomainSettings"));
             services.AddTransient<StoreService>();
-            //services.AddTransient<ProductService>();
-            //services.AddTransient<ProviderService>();
+            services.AddTransient<ProductService>();
+            // services.AddTransient<ProviderService>();
             services.AddTransient<ProductTypeService>();
             services.AddTransient<Repository.LiteDb.Configuration.ConfigurationProvider>();
             services.AddTransient<ILiteConfiguration, LiteConfiguration>();

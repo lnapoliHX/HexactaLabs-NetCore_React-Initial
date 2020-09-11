faaf101d1e9d8e5ca54dac910fd98fd7ccfeb66d
WANA GROUPS <57154870+wanagroups@users.noreply.github.com>
2020-09-11 14:19:58 -0300

    git add all

---
+++ Stock.Api/DTOs/ProviderSearchDTO.cs
@@ -0,0 +1,11 @@
+namespace Stock.Api.DTOs
+{
+    public class ProviderSearchDTO
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public ActionDto Phone { get; set; } 
+    }
+}

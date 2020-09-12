a33b02235acc5ac13807d46d8b352102d0240c51
Gabriel Andres Brandalisse <gabibrandalisse@gmail.com>
2020-09-11 21:18:30 -0300

    corrigiendo observaciones

--- Stock.Api/DTOs/ProviderDTO.cs
+++ Stock.Api/DTOs/ProviderDTO.cs
@@ -1,14 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Stock.Model.Entities;
 
 namespace Stock.Api.DTOs
 {
     public class ProviderDTO
     {
-        public string Id { get; set; }
-
+        [Required]
         public string Name { get; set; }
+        public string Id { get; set; }
 
         public string Phone { get; set; }
 

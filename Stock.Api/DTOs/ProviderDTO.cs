c16f7af94088d41ce503e58d086e781a4947d889
dana-halle <danaha17@gmail.com>
2020-09-12 15:28:38 -0300

    entrega inicial Dana Halle

---
+++ Stock.Api/DTOs/ProviderDTO.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Stock.Api.DTOs
+{
+    public class ProviderDTO
+    {
+        [Required]
+        public string Name { get; set; }
+        
+        public string Id { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public List<ProductDTO> OfferedProducts { get; set; }
+    }
+}

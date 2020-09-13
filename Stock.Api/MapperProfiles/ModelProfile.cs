fefd097566c4048d5c93c626c6efef8ceecd9fc3
unknown <juandimatz@gmail.com>
2020-09-13 19:07:46 -0300

    Actividad inicial terminada

--- Stock.Api/MapperProfiles/ModelProfile.cs
+++ Stock.Api/MapperProfiles/ModelProfile.cs
@@ -13,15 +13,21 @@ namespace Stock.Api.MapperProfiles
                 .ReverseMap()
                 .ForMember(s => s.Id, opt => opt.Ignore());
 
-            // CreateMap<Product, ProductDTO>()
-            //     .ForMember(d => d.ProductTypeId, opt => opt.MapFrom(s => s.ProductType.Id))
-            //     .ForMember(d => d.ProductTypeDesc, opt => opt.MapFrom(s => s.ProductType.Description))
-            //     .ReverseMap()
-            //     .ForMember(s => s.Id, opt => opt.Ignore())
-            //     .ForMember(s => s.ProductType, opt => opt.Ignore());       
+            //CreateMap<Product, ProductDTO>()
+                //.ForMember(d => d.ProductTypeId, opt => opt.MapFrom(s => s.ProductType.Id))
+                //.ForMember(d => d.ProductTypeDesc, opt => opt.MapFrom(s => s.ProductType.Description))
+                //.ForMember(d => d.ProductType.Id, opt => opt.MapFrom(s => s.ProductType.Id))
+                //.ForMember(d => d.ProductType.Description, opt => opt.MapFrom(s => s.ProductType.Description))
+                //.ReverseMap()
+                //.ForMember(s => s.Id, opt => opt.Ignore())
+                //.ForMember(s => s.ProductType, opt => opt.Ignore());       
 
-            // CreateMap<Provider, ProviderDTO>()
-            //     .ReverseMap();         
+            CreateMap<Provider, ProviderDTO>()
+                //.ForMember(d => d.Name, opt => opt.MapFrom(s => s.Name))
+                //.ForMember(d => d.Phone, opt => opt.MapFrom(s => s.Phone))
+                //.ForMember(d => d.Email, opt => opt.MapFrom(s => s.Email))
+                .ReverseMap();
+                //.ForMember(s => s.Id, opt => opt.Ignore());         
         }        
     }
 

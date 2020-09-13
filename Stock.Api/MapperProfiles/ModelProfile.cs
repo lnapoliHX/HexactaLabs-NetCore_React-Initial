da0616d681ae2464ad926e7cdb400c057581e86c
unknown <juandimatz@gmail.com>
2020-09-13 19:13:09 -0300

    comentarios borrados

--- Stock.Api/MapperProfiles/ModelProfile.cs
+++ Stock.Api/MapperProfiles/ModelProfile.cs
@@ -23,11 +23,7 @@ namespace Stock.Api.MapperProfiles
                 //.ForMember(s => s.ProductType, opt => opt.Ignore());       
 
             CreateMap<Provider, ProviderDTO>()
-                //.ForMember(d => d.Name, opt => opt.MapFrom(s => s.Name))
-                //.ForMember(d => d.Phone, opt => opt.MapFrom(s => s.Phone))
-                //.ForMember(d => d.Email, opt => opt.MapFrom(s => s.Email))
-                .ReverseMap();
-                //.ForMember(s => s.Id, opt => opt.Ignore());         
+                .ReverseMap();         
         }        
     }
 

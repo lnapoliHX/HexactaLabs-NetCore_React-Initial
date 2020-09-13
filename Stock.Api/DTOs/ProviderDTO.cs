dfe39531b4021a6e1ded69d79cebc53e35cdee62
veronfederico45 <veronfederico45@gmail.com>
2020-09-13 18:10:08 -0300

    actividad inicial

---
+++ Stock.Api/DTOs/ProviderDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using Stock.Model.Entities;
+
+namespace Stock.Api.DTOs
+{
+    public class ProviderDTO
+    {
+        
+        public string Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        
+       public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public List<Product> OfferedProducts { get; set; }
+
+    }
+}

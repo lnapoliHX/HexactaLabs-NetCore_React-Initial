aab5561a7479ac552f380060efe2203c271c115f
SofiArz <sofia.arzamendia@hotmail.es>
2020-09-13 16:17:31 -0300

    search and delete provider methods

--- Stock.Api/Controllers/ProviderController.cs
+++ Stock.Api/Controllers/ProviderController.cs
@@ -57,6 +57,42 @@ namespace Stock.Api.Controllers
             }
         }
 
+        /// <summary>
+        /// Permite borrar una instancia
+        /// </summary>
+        /// <param name="id">Identificador de la instancia a borrar</param>
+        [HttpDelete("{id}")]
+        public ActionResult Delete(string id)
+        {
+            var provider = this.service.Get(id);
+
+            this.service.Delete(provider);
+            return Ok(new { Success = true, Message = "", data = id });
+        }
+
+        [HttpPost("search")]
+        public ActionResult Search([FromBody] ProviderSearchDTO model)
+        {
+            Expression<Func<Provider, bool>> filter = x => !string.IsNullOrWhiteSpace(x.Id);
+
+            if (!string.IsNullOrWhiteSpace(model.Name))
+            {
+                filter = filter.AndOrCustom(
+                    x => x.Name.ToUpper().Contains(model.Name.ToUpper()),
+                    model.Condition.Equals(ActionDto.AND));
+            }
+
+            if (!string.IsNullOrWhiteSpace(model.Email))
+            {
+                filter = filter.AndOrCustom(
+                    x => x.Email.ToUpper().Contains(model.Email.ToUpper()),
+                    model.Condition.Equals(ActionDto.AND));
+            }
+
+            var stores = this.service.Search(filter);
+            return Ok(stores);
+        }
+
 
         
     }

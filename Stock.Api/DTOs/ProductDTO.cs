e723e944d82ae0fe3d6416e2dfde689c035df025
Agustina Superi <agusuperi@gmail.com>
2020-09-13 11:55:20 -0300

    Entrega inicial finalizada
    Se realizo el CRUD de la clase Provider.
    Se puede buscar un provider por nombre o email.
    La interfaz swagge muestra los servicios que ofrece la clase Provider.

---
+++ Stock.Api/DTOs/ProductDTO.cs
@@ -0,0 +1,25 @@
+﻿using Stock.Model.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stock.Api.DTOs
+{
+    public class ProductDTO
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal CostPrice { get; set; }
+
+        public decimal SalePrice { get; set; }
+
+        public virtual ProductTypeDTO ProductType { get; set; }
+
+        public string ProviderId { get; set; }
+
+        public ProviderDTO Provider { get; set; }
+    }
+}

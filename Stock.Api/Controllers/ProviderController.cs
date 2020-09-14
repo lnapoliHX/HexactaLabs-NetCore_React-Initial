80440ba07de8a89cc383d690c1fc6b010c8532a3
Eduardo Jaime <edudjaime@gmail.com>
2020-09-13 22:36:15 -0300

    EJ - actividadInicial - add Controller DTO and Service for Provider

---
+++ Stock.Api/Controllers/ProviderController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Stock.Api.DTOs;
+using Stock.Api.Extensions;
+using Stock.AppService.Services;
+using Stock.Model.Entities;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Stock.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProviderController : ControllerBase
+    {
+        private ProviderService service;
+        private readonly IMapper mapper;
+
+        public ProviderController(ProviderService service, IMapper mapper)
+        {
+            this.service = service;
+            this.mapper = mapper;
+        }
+
+        // GET: api/<ProviderController>
+        [HttpGet]
+        public ActionResult<IEnumerable<ProviderDTO>> Get()
+        {
+            try
+            {
+                var result = this.service.GetAll();
+                return this.mapper.Map<IEnumerable<ProviderDTO>>(result).ToList();
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        // GET api/<ProviderController>/5
+        [HttpGet("{id}")]
+        public ActionResult<ProviderDTO> Get(string id)
+        {
+            try
+            {
+                var result = this.service.Get(id);
+                return this.mapper.Map<ProviderDTO>(result);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        // POST api/<ProviderController>
+        [HttpPost]
+        public ActionResult Post([FromBody] ProviderDTO value)
+        {
+            TryValidateModel(value);
+            try
+            {
+                var provider = this.mapper.Map<Provider>(value);
+                this.service.Create(provider);
+                value.Id = provider.Id;
+                return Ok(new { Success = true, Message = "", data = value });
+            }
+            catch
+            {
+                return Ok(new { Success = false, Message = "Ups! Something Happened, Sorry" });
+            }
+        }
+
+        // PUT api/<ProviderController>/5
+        [HttpPut("{id}")]
+        public void Put(string id, [FromBody] ProviderDTO value)
+        {
+            var provider = this.service.Get(id);
+            TryValidateModel(value);
+            this.mapper.Map<ProviderDTO, Provider>(value, provider);
+            this.service.Update(provider);
+        }
+
+        // DELETE api/<ProviderController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(string id)
+        {
+            var store = this.service.Get(id);
+
+            this.service.Delete(store);
+            return Ok(new { Success = true, Message = "Delete Complete", data = id });
+        }
+
+        [HttpPost("search")]
+        public ActionResult Search([FromBody] ProviderSearchDTO model)
+        {
+            Expression<Func<Provider, bool>> filter = x => !string.IsNullOrWhiteSpace(x.Id);
+
+            if (!string.IsNullOrWhiteSpace(model.Name))
+            {
+                filter = filter.AndOrCustom(
+                    x => x.Name.ToUpper().Contains(model.Name.ToUpper()),
+                    model.Condition.Equals(ActionDto.AND));
+            }
+
+            if (!string.IsNullOrWhiteSpace(model.Email))
+            {
+                filter = filter.AndOrCustom(
+                    x => x.Email.ToUpper().Contains(model.Email.ToUpper()),
+                    model.Condition.Equals(ActionDto.AND));
+            }
+
+            var providers = this.service.Search(filter);
+            return Ok(providers);
+        }
+    }
+}
---
+++ Stock.AppService/Services/ProviderService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Stock.AppService.Base;
+using Stock.Model.Entities;
+using Stock.Repository.LiteDb.Interface;
+
+namespace Stock.AppService.Services
+{
+    public class ProviderService : BaseService<Provider>
+    {        
+        public ProviderService(IRepository<Provider> repository) : base(repository)
+        {
+
+        }
+
+        public new Provider Create(Provider entity)
+        {           
+            return base.Create(entity);  
+        }
+
+        public IEnumerable<Provider> Search(Expression<Func<Provider, bool>> filter)
+        {
+            return this.Repository.List(filter);
+        }
+    }
+}

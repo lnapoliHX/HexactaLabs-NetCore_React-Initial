b8bfe969e41797a1bac336f6effcfb68609bd1ce
joaquincordero <jgcordero01@gmail.com>
2020-09-13 22:38:07 -0300

    Actividad inicial Proveedores

---
+++ Stock.Api/DTOs/ProvidersDTO.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+namespace Stock.Api.DTOs
+{
+    public class ProvidersDTO
+    {
+         [Required]
+        public string Name { get; set; }
+
+        public string Id { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+       
+    }
+}

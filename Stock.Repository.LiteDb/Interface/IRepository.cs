2e33ed47131dc02d989ad4c4d1804be7d5e16d68
undefined-procedure <gomez.aita@hotmail.com>
2020-09-14 04:31:57 -0300

    rollback to IRepository.cs

--- Stock.Repository.LiteDb/Interface/IRepository.cs
+++ Stock.Repository.LiteDb/Interface/IRepository.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
 using Stock.Model.Base;
 
@@ -8,7 +7,7 @@ namespace Stock.Repository.LiteDb.Interface
 {
     public interface IRepository<T> where T : IEntity
     {
-        T GetById(string id);     
+        T GetById(string id);
         IReadOnlyList<T> List(Expression<Func<T, bool>> filter = null);
         IReadOnlyList<T> ListLimit(Expression<Func<T, bool>> filter = null, int size = 15);
         T Add(T entity);

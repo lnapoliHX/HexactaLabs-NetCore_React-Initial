d196083d789f13ff40064fa08ae2094fbfff11e6
delforhelver <delfor_10_11@hotmail.es>
2020-09-14 00:46:08 -0300

    EntregaEtapaInicial

---
+++ Stock.Api/DTOs/ProviderSearchDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Stock.Api.DTOs
+{
+    public class ProviderSearchDTO
+    {
+        public string Email { get; set; }
+
+        public string Name { get; set; }
+
+        public ActionDto Condition { get; set; }
+    }
+}

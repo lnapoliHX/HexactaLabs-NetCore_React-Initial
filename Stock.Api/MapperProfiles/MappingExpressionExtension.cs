c5f2acf243078c281b529e2f418bc2d870da3459
pgoette <pgoette@hexacta.com>
2021-06-23 09:57:10 -0300

    migración a net 5

--- Stock.Api/MapperProfiles/MappingExpressionExtension.cs
+++ Stock.Api/MapperProfiles/MappingExpressionExtension.cs
@@ -8,18 +8,19 @@ namespace Stock.Api.MapperProfiles
 {
     public static class MappingExpressionExtension
     {
-        public static IMappingExpression<TSource, TDestination> IgnoreAllNonExisting<TSource, TDestination>
+     /*   public static IMappingExpression<TSource, TDestination> IgnoreAllNonExisting<TSource, TDestination>
             (this IMappingExpression<TSource, TDestination> expression)
         {
             var sourceType = typeof(TSource);
             var destinationType = typeof(TDestination);
 
-            var existingMaps = Mapper.Configuration.GetAllTypeMaps().First(x => x.SourceType.Equals(sourceType) && x.DestinationType.Equals(destinationType));
+            var existingMaps = AutoMapper.Configuration.GetAllTypeMaps()
+            .First(x => x.SourceType.Equals(sourceType) && x.DestinationType.Equals(destinationType));
 
             foreach (var property in existingMaps.GetUnmappedPropertyNames())
                 expression.ForMember(property, opt => opt.Ignore());
 
             return expression;
-        }
+        }*/
     }
 }

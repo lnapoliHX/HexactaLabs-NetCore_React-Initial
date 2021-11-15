a8b05ad7f8c43d449d9ec1703053f9666e22d259
Luis Acuna <ldacuna83@gmail.com>
2021-11-15 15:06:31 -0500

    Providers addedprueba

---
+++ Stock.AppService/Services/ProviderService.cs
@@ -0,0 +1,64 @@
+﻿using Stock.AppService.Base;
+using Stock.Model.Entities;
+using Stock.Repository.LiteDb.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Stock.AppService.Services
+{
+    /// <summary>
+    /// Provider Service.
+    /// </summary>
+    public class ProviderService : BaseService<Provider>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProviderService"/> class.
+        /// </summary>
+        /// <param name="repository">Provider Repository.</param>
+        public ProviderService(IRepository<Provider> repository) : base(repository)
+        {
+        }
+
+        /// <summary>
+        /// Creates a provider.
+        /// </summary>
+        /// <param name="entity">Provider information.</param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public new Provider Create(Provider entity)
+        {
+            if (NombreUnico(entity.Name))
+            {
+                return base.Create(entity);
+            }
+
+            throw new Exception("The name is already in use");
+        }
+
+        /// <summary>
+        /// Checks if the provider name is unique or not.
+        /// </summary>
+        /// <param name="name">Provider name to check.</param>
+        /// <returns></returns>
+        private bool NombreUnico(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return false;
+            }
+
+            return Repository.List(x => x.Name.ToUpper().Equals(name.ToUpper())).Count == 0;
+        }
+
+        /// <summary>
+        /// Search providers.
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        public IEnumerable<Provider> Search(Expression<Func<Provider, bool>> filter)
+        {
+            return Repository.List(filter);
+        }
+    }
+}

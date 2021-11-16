c8a097b66d3e62ac09a548a2eb49b7c20d3aa3d0
Adrian De Simone <adesimone@mpublico.gov.ar>
2021-11-16 15:27:43 -0300

    Actividad Inicial

---
+++ Stock.AppService/Services/ProviderService.cs
@@ -0,0 +1,21 @@
+﻿using Stock.AppService.Base;
+using Stock.Model.Entities;
+using Stock.Repository.LiteDb.Interface;
+
+namespace Stock.AppService.Services
+{
+    /// <summary>
+    /// Provider service.
+    /// </summary>
+    public class ProviderService : BaseService<Provider>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProviderService"/> class.
+        /// </summary>
+        /// <param name="repository">Provider repository.</param>
+        public ProviderService(IRepository<Provider> repository)
+            : base(repository)
+        {
+        }
+    }
+}

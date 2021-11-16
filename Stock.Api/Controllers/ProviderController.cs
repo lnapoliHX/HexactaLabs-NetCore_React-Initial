6481c6f4a757b63ff59e342499ab4962a86404a2
Adrian Rodriguez <Appxims@gmail.com>
2021-11-16 19:58:57 -0300

    test ok 1,2,3,5

---
+++ Stock.Api/Controllers/ProviderController.cs
@@ -0,0 +1,255 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Stock.Api.DTOs;
+using Stock.Api.Extensions;
+using Stock.Api.MapperProfiles;
+using Stock.AppService.Services;
+using Stock.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Stock.Api.Controllers
+{
+    /// <summary>
+    /// Store endpoint.
+    /// </summary>
+    [Produces("application/json")]
+    [Route("api/provider")]
+    [ApiController]
+    public class ProviderController : ControllerBase
+    {
+        private readonly ProviderService service;
+
+        private readonly ILogger<ProviderController> logger;
+        private readonly IMapper mapper;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProviderController"/> class.
+        /// </summary>
+        /// <param name="service">Store service.</param>
+        /// <param name="mapper">Mapper configurator.</param>
+        /// <param name="logger">Logger service.</param>
+        public ProviderController(ProviderService service, IMapper mapper, ILogger<ProviderController > logger)
+        {
+            this.service = service ?? throw new ArgumentException(nameof(service));
+            this.mapper = mapper ?? throw new ArgumentException(nameof(mapper));
+            this.logger = logger ?? throw new ArgumentException(nameof(logger));
+        }
+
+        /// <summary>
+        /// Deletes a provider.
+        /// </summary>
+        /// <param name="id">provider id to delete</param>
+        [HttpDelete("{id}")]
+        public ActionResult Delete(string id)
+        {
+            var store = service.Get(id);
+            if (store is null)
+                return NotFound();
+
+            service.Delete(store);
+            return Ok(new { Success = true, Message = "", data = id });
+        }
+
+
+        /// <summary>
+        /// Gets all providers.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult<IEnumerable<ProviderDTO>> Get()
+        {
+            try
+            {
+                var result = service.GetAll();
+                return mapper.Map<IEnumerable<ProviderDTO>>(result).ToList();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500);
+            }
+        }
+
+        /// <summary>
+        /// Gets a store by id.
+        /// </summary>
+        /// <param name="id">Store id.</param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public ActionResult<ProviderDTO> Get(string id)
+        {
+            try
+            {
+                var result = service.Get(id);
+                return mapper.Map<ProviderDTO>(result);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500);
+            }
+        }
+
+        /// <summary>
+        /// Updates a provider.
+        /// </summary>
+        /// <param name="id">provider id.</param>
+        /// <param name="value">provider information.</param>
+        [HttpPut("{id}")]
+        public void Put(string id, [FromBody] ProviderDTO value)
+        {
+            var provider = service.Get(id);
+            TryValidateModel(value);
+            mapper.Map<ProviderDTO,Provider >(value, provider);
+            service.Update(provider);
+        }
+
+
+        /// <summary>
+        /// Search some stores.
+        /// </summary>
+        /// <param name="model">Store filters.</param>
+        /// <returns></returns>
+        [HttpPost("search")]
+        public ActionResult Search([FromBody] ProviderSearchDTO model)
+        {
+            Expression<Func<Provider, bool>> filter = x => !string.IsNullOrWhiteSpace(x.Id);
+
+            if (!string.IsNullOrWhiteSpace(model.Name))
+            {
+                filter = filter.AndOrCustom(
+                    x => x.Name.ToUpper().Contains(model.Name.ToUpper()),
+                    model.Condition.Equals(ActionDto.AND));
+            }
+
+            if (!string.IsNullOrWhiteSpace(model.Phone))
+            {
+                filter = filter.AndOrCustom(
+                    x => x.Phone.ToUpper().Contains(model.Phone.ToUpper()),
+                    model.Condition.Equals(ActionDto.AND));
+            }
+            if (!string.IsNullOrWhiteSpace(model.Email))
+            {
+                filter = filter.AndOrCustom(
+                    x => x.Email.ToUpper().Contains(model.Email.ToUpper()),
+                    model.Condition.Equals(ActionDto.AND));
+            }
+
+
+            var stores = service.Search(filter);
+            return Ok(stores);
+        }
+
+
+        /// <summary>
+        /// Adds a Provider.
+        /// </summary>
+        /// <param name="value">provider info.</param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Post([FromBody] ProviderDTO value)
+        {
+            TryValidateModel(value);
+
+            try
+            {
+                var store = mapper.Map<Provider>(value);
+                service.Create(store);
+                value.Id = store.Id;
+                return Ok(new { Success = true, Message = "", data = value });
+            }
+            catch (Exception ex)
+            {
+                logger.LogCritical(ex.StackTrace);
+                return Ok(new { Success = false, Message = "The name is already in use" });
+            }
+        }
+
+
+
+
+        // GET: ProviderController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ProviderController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProviderController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProviderController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProviderController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProviderController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProviderController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProviderController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        private ActionResult View()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Stock.Api/DTOs/ProviderDTO.cs
+++ Stock.Api/DTOs/ProviderDTO.cs
@@ -1,6 +1,20 @@
-﻿namespace Stock.Api.MapperProfiles
+﻿using Stock.Model.Entities;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Stock.Api.MapperProfiles
 {
-    internal class ProviderDTO
+    public  class ProviderDTO
     {
+        [Required]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public List<Product> OfferedProducts { get; set; }
     }
 }

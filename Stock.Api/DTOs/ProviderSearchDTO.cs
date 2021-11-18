d2bb4c3bde8a2e1500858f39a776aa310edeaf14
Adrian De Simone <adesimone@mpublico.gov.ar>
2021-11-18 10:55:15 -0300

    Actividad Inicial (finalizada)

---
+++ Stock.Api/DTOs/ProviderSearchDTO.cs
@@ -0,0 +1,13 @@
+namespace Stock.Api.DTOs
+{
+    public class ProviderSearchDTO
+    {
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public ActionDto Condition { get; set; }
+    }
+}

a510ab33e2e4eca7f236a2bb28457cd0fc23637d
Radamés Ibañez <soyradames@gmail.com>
2021-11-19 17:05:12 -0300

    Primera versión de la actividad inicial para revisión.

---
+++ Stock.Api/DTOs/ProviderSearchDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Stock.Api.DTOs
+{
+    public class ProviderSearchDTO
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+
+        public ActionDto Condition { get; set; }
+    }
+}

04ff57fc7487802a692481e90c565e6e1a79d452
DerivadaDX <derivadadx@gmail.com>
2021-11-20 18:20:12 -0300

    Se agregó Required a Phone e Email de ProviderDTO.

--- Stock.Api/DTOs/ProviderDTO.cs
+++ Stock.Api/DTOs/ProviderDTO.cs
@@ -9,8 +9,10 @@ namespace Stock.Api.DTOs
         [Required]
         public string Name { get; set; }
 
+        [Required]
         public string Phone { get; set; }
 
+        [Required]
         public string Email { get; set; }
     }
 }

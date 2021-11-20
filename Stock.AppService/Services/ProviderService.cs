a96dc7c34a8690feeeebc618ae5b31ecd78ad50f
DerivadaDX <derivadadx@gmail.com>
2021-11-19 21:25:40 -0300

    Se implementó ProviderService.

---
+++ Stock.AppService/Services/ProviderService.cs
@@ -0,0 +1,17 @@
+﻿using Stock.AppService.Base;
+using Stock.Model.Entities;
+using Stock.Repository.LiteDb.Interface;
+
+namespace Stock.AppService.Services
+{
+    public class ProviderService : BaseService<Provider>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProviderService"/> class.
+        /// </summary>
+        /// <param name="repository">Provider repository.</param>
+        public ProviderService(IRepository<Provider> repository) : base(repository)
+        {
+        }
+    }
+}

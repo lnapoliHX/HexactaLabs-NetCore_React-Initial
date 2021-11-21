35f255fdef392754a908257ccd352362b382fb03
tspignoux <tspignoux@gmail.com>
2021-11-21 17:38:50 -0300

    todo andando

---
+++ Stock.Api/DTOs/ProviderSearchDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Stock.Api.DTOs
+{
+    public class ProviderSearchDTO
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public ActionDto Action { get; set; }
+    }
+}

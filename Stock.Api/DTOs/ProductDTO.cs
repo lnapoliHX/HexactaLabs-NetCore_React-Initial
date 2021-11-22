0c2c8c86179a501e3a25697fba3f00312780dda2
earosselot <earosselot@gmail.com>
2021-11-22 15:26:07 -0300

    Added Provider

---
+++ Stock.Api/DTOs/ProductDTO.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Stock.Api.DTOs
+{
+    public class ProductDTO
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal CostPrice { get; set; }
+
+        public decimal SalePrice { get; set; }
+
+        public virtual ProductTypeDTO ProductType { get; set; }
+
+        private int _stock;
+
+        public int Stock => _stock;
+
+        public string ProviderId { get; set; }
+
+        public ProviderDTO Provider { get; set; }
+    }
+}
---
+++ Stock.AppService/Services/ProviderService.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Stock.AppService.Base;
+using Stock.Model.Entities;
+using Stock.Repository.LiteDb.Interface;
+
+namespace Stock.AppService.Services
+{
+    /// <summary>
+    /// Provider service.
+    /// </summary>
+    public class ProviderService : BaseService<Provider>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProviderService"/> class.
+        /// </summary>
+        /// <param name="repository">Provider repository.</param>
+        public ProviderService(IRepository<Provider> repository) 
+            : base(repository) 
+        {    
+        }
+
+        /// <summary>
+        /// Creates a provider.
+        /// </summary>
+        /// <param name="entity">Provider information.</param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception> 
+        public new Provider Create(Provider entity)
+        {
+            if (NombreUnico(entity.Name))
+            {
+                return base.Create(entity);
+            }
+            throw new Exception("Name already in use");
+        }
+
+        /// <summary>
+        /// Checks if the store name is unique or not.
+        /// </summary>
+        /// <param name="name">Store name to check.</param>
+        /// <returns></returns>
+        private bool NombreUnico(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return false;
+            }
+
+            return Repository.List(x => x.Name.ToUpper().Equals(name.ToUpper())).Count == 0;
+        }
+    }
+}

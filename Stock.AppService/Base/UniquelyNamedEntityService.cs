6bdd5843c69c45a84844f609f72d785d610a98a6
DerivadaDX <derivadadx@gmail.com>
2021-11-23 22:33:51 -0300

    Se creó UniquelyNamedEntityService.

---
+++ Stock.AppService/Base/UniquelyNamedEntityService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Stock.Model.Base;
+using Stock.Repository.LiteDb.Interface;
+
+namespace Stock.AppService.Base
+{
+    public abstract class UniquelyNamedEntityService<TEntity> : BaseService<TEntity> where TEntity : class, IUniquelyNamedEntity
+    {
+        protected UniquelyNamedEntityService(IRepository<TEntity> repository) : base(repository)
+        {
+        }
+        public new TEntity Create(TEntity entity)
+        {
+            if (IsUnique(entity.Name))
+            {
+                return base.Create(entity);
+            }
+
+            throw new Exception($"The entity name is not unique: {entity.Name}.");
+        }
+
+        protected bool IsUnique(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                return false;
+
+            var entitiesWithSameName = Repository.List(x => x.Name.ToUpper().Equals(name.ToUpper()));
+
+            return entitiesWithSameName.Count == 0;
+        }
+    }
+}

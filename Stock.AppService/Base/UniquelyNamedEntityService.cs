3468771036469630373d133b8355337c1a3d135b
DerivadaDX <derivadadx@gmail.com>
2021-11-23 22:34:11 -0300

    Se agregó summary a UniquelyNamedEntityService.

--- Stock.AppService/Base/UniquelyNamedEntityService.cs
+++ Stock.AppService/Base/UniquelyNamedEntityService.cs
@@ -4,11 +4,27 @@ using Stock.Repository.LiteDb.Interface;
 
 namespace Stock.AppService.Base
 {
+    /// <summary>
+    /// Service for entities whose name must be unique.
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
     public abstract class UniquelyNamedEntityService<TEntity> : BaseService<TEntity> where TEntity : class, IUniquelyNamedEntity
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UniquelyNamedEntityService{TEntity}"/> class.
+        /// </summary>
+        /// <param name="repository">Generic repository.</param>
+        /// <exception cref="ArgumentNullException"></exception>
         protected UniquelyNamedEntityService(IRepository<TEntity> repository) : base(repository)
         {
         }
+
+        /// <summary>
+        /// Creates a new uniquely named item.
+        /// </summary>
+        /// <param name="entity">Item information.</param>
+        /// <returns>The newly created <see cref="TEntity"/>.</returns>
+        /// <exception cref="Exception"></exception>
         public new TEntity Create(TEntity entity)
         {
             if (IsUnique(entity.Name))

631afd881bd9c9b9bf9aab420420fcccf80908a2
DerivadaDX <derivadadx@gmail.com>
2021-11-23 22:42:33 -0300

    ProviderService implementa UniquelyNamedEntityService. Se borraron métodos Create y NameIsUnique.

--- Stock.AppService/Services/ProviderService.cs
+++ Stock.AppService/Services/ProviderService.cs
@@ -7,7 +7,7 @@ using System.Linq.Expressions;
 
 namespace Stock.AppService.Services
 {
-    public class ProviderService : BaseService<Provider>
+    public class ProviderService : UniquelyNamedEntityService<Provider>
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="ProviderService"/> class.
@@ -15,34 +15,6 @@ namespace Stock.AppService.Services
         /// <param name="repository">Provider repository.</param>
         public ProviderService(IRepository<Provider> repository) : base(repository)
         {
-        }
-
-        /// <summary>
-        /// Creates a provider.
-        /// </summary>
-        /// <param name="provider">Provider information.</param>
-        /// <returns>A <see cref="Provider"/>.</returns>
-        /// <exception cref="Exception"></exception>
-        public new Provider Create(Provider provider)
-        {
-            if (NameIsUnique(provider.Name))
-            {
-                return base.Create(provider);
-            }
-
-            throw new Exception("The name is already in use");
-        }
-
-        private bool NameIsUnique(string name)
-        {
-            if (string.IsNullOrWhiteSpace(name))
-            {
-                return false;
-            }
-
-            var providersWithSameName = Repository.List(x => x.Name.ToUpper().Equals(name.ToUpper()));
-
-            return providersWithSameName.Count == 0;
         }
 
         /// <summary>
